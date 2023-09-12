793e866445db9327e325864ca3d246ac5efa6311
Jacob Marinas <jacob1marinas@gmail.com>
2023-09-12 16:00:14 -0700

    Added psuedocode

---
+++ streetSmart/streetSmart.ino
@@ -0,0 +1,32 @@
+// AvenueE 2023 Team 14 StreetSmart Detect and Avoid Project
+// Authors: Jacob Marinas, Tian Hong Zhu Zhou (Alex), Ruben Leon
+// Date: 09/12/2023
+
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Drive subsystem
+  // Uses joystick, buttons, and dc motors
+  // Manages driver input and driving output
+
+  // Collect distance value for Ultra Sonic Distance Sensor (for all three UDS)
+  // Store boolean if distance is below certain threshold (for front, left, and right sensor)
+
+  // 
+  // Tone
+  // Delay
+  // no tone
+  // delay
+
+  // Avoid subsystem
+  // Uses ultrasonic distance sensor, dc motors, LED
+  // If the vehicle is close to an obstacle, it will override driver input, and react to avoid the obstacle accordingly,
+  // turning on an LED to indicate that it is overriding controls
+
+  }
